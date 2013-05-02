3a5a2a5e0c63c6c13d323a91649d9fe0ca4de66c
Andy Pliszka <andy@isp44.com>
2013-05-01 22:59:34 -0400

    Adds ctags and path modification

--- cookbooks/tools/recipes/default.rb
+++ cookbooks/tools/recipes/default.rb
@@ -1,1 +1,9 @@
 package "hub"
+package "ctags-exuberant"
+
+bash "setup_path" do
+  home = "#{ENV['HOME']}"
+  code <<-EOH
+  echo 'export PATH=/usr/local/bin:$PATH' > #{home}/.workstationrc
+  EOH
+end
